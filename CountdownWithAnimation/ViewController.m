5a1f1b3bdbb75486c6ffb7eeb55018a04fea975c
NixonShih <powerwolf543@gmail.com>
2016-05-26 23:30:13 +0800

    Create Project

--- CountdownWithAnimation/ViewController.m
+++ CountdownWithAnimation/ViewController.m
@@ -8,20 +8,55 @@
 
 #import "ViewController.h"
 
+#define kSecond 10
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *secondLabel;
 
 @end
 
 @implementation ViewController
+{
+    NSInteger countdownTimeNum;
+    BOOL isAnimation;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _secondLabel.text = @"";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)countdownAnimationWithLabel:(UILabel*)label {
+    label.transform = CGAffineTransformIdentity;
+    label.text = [NSString stringWithFormat:@"%ld",(long)countdownTimeNum];
+    label.alpha = 1;
+    
+    [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        label.alpha = 0;
+        label.transform = CGAffineTransformScale(label.transform, 0.1, 0.1);
+    } completion:^(BOOL finished) {
+        if (!(countdownTimeNum <= 1) && isAnimation) {
+            countdownTimeNum -= 1;
+            [self countdownAnimationWithLabel:label];
+        }
+    }];
+}
+
+- (IBAction)startCountdown:(id)sender {
+    if (isAnimation) {
+        isAnimation = false;
+        [_secondLabel.layer removeAllAnimations];
+        _secondLabel.text = @"";
+    }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+        countdownTimeNum = kSecond;
+        _secondLabel.text = [NSString stringWithFormat:@"%ld",(long)countdownTimeNum];
+        isAnimation = true;
+        [self countdownAnimationWithLabel:_secondLabel];
+    });
+
 }
 
 @end
